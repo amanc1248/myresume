d56983ee4565cbb7b27da608fcaf8dc4d8842d55
amanc1248 <amanc1248@gmail.com>
2021-03-08 08:23:04 +0545

    ADD: Resume

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOy8M9TS0sOc8kDpTMs2x5FVxsQiQHy5k",
+  authDomain: "amanporfolio.firebaseapp.com",
+  projectId: "amanporfolio",
+  storageBucket: "amanporfolio.appspot.com",
+  messagingSenderId: "761631592612",
+  appId: "1:761631592612:web:d19917e5f4166a8c415631",
+  measurementId: "G-J18EYMS46P",
+};
